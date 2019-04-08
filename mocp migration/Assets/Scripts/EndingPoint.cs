53021487c45dd2d4e6829ff21029ac9e234eb7f0
gemna117 <megan.alonzo@loop.colum.edu>
2019-04-08 11:08:49 -0700

    starting scripts

---
+++ mocp migration/Assets/Scripts/EndingPoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingPoint : MonoBehaviour
+{
+    public Texture2D zoomedInTexture;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        GameObject.Find("map").GetComponent<Renderer>().material.mainTexture = zoomedInTexture;
+    }
+}
---
+++ mocp migration/Assets/Scripts/StartingPoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartingPoint : MonoBehaviour
+{
+    public Texture2D zoomedInTexture;
+    private List<string> maplocations;
+    List<GameObject> locations = new List<GameObject>();
+    //ive learned im better at programming when i have example code to use as reference
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        GameObject.Find("map").GetComponent<Renderer>().material.mainTexture = zoomedInTexture;
+    }
+}
